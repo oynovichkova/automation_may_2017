335700074c1547640b262176cfd81aae4c9f1c90
oynovichkova <oynovichkova1811@yandex.ru>
2017-05-22 11:31:40 +0300

    Add files via upload

---
+++ my own api.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, abort
+from flask import render_template
+from flask import request
+import json
+
+app = Flask(__name__)
+
+recipies = [
+    {
+        "name":"fried eggs",
+     "ingridients": "eggs, butter, fire",
+     "recipy": "just crush eggs",
+    "id":1
+     },
+
+    {
+        "name":"cake",
+     "ingridients": "a lot of cake staff",
+     "recipy": "just crush it together and put in the store",
+     "id":2
+     }
+]
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+@app.route('/hello/', methods=['POST', 'GET'])
+def hello():
+    name=input(['your name'])
+    recip=input(['your favorite recipy'])
+    if recip == 'fried eggs':
+        return jsonify({'recipy':recipies[0]})
+    elif recip == 'cake':
+        return jsonify({'recipy': recipies[1]})
+    else:
+        return name + " you want too much"
+
+
+@app.route('/pers/api/v1/list/<int:pers_id>', methods=['GET'])
+def list_recipy_by_id(res_id):
+    persona = filter(lambda t: t['id']== pers_id, recipies)
+    if persona == 0:
+        abort(404)
+    return jsonify({'recipy':recipies[0]})
+
+@app.route('/pers/api/v1/list/', methods=['GET'])
+def list_recipy_all():
+    return jsonify({'recipy':recipies})
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=8882)
