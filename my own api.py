4f23e9852a3a35aa3180c54620a025614dcdb9e6
oynovichkova <oynovichkova1811@yandex.ru>
2017-05-23 14:21:13 +0300

    Update my own api.py

--- my own api.py
+++ my own api.py
@@ -25,33 +25,49 @@ recipies = [
 def index():
     return "Hello world"
 
+#@app.route('/hello/', methods=['POST', 'GET'])
+#def hello():
+#    name=input(['your name'])
+#    recip=input(['your favorite recipy'])
+#    if recip == 'fried eggs':
+#        return jsonify({'recipy':recipies[0]})
+#    elif recip == 'cake':
+#        return jsonify({'recipy': recipies[1]})
+#   else:
+#        return name + " you want too much"
+
 @app.route('/hello/', methods=['POST', 'GET'])
 def hello():
-    name=input(['your name'])
-    recip=input(['your favorite recipy'])
-    if recip == 'fried eggs':
-        return jsonify({'recipy':recipies[0]})
-    elif recip == 'cake':
-        return jsonify({'recipy': recipies[1]})
-    else:
+    name = input(['your name'])
+    recip = input(['your favorite recipy'])
+    recipy = [recipy for recipy in recipies
+              if recipy['name'] == recip]
+    if len(recipy) == 0:
         return name + " you want too much"
+    elif recipy != None:
+        return jsonify(recipy)
+#    else:
+#        return name + " you want too much"
 
+#@app.route('/pers/api/v1/list/<int:res_id>', methods=['GET'])
+#def list_recipy_by_id(res_id):
+#    for recipy in recipies:
+#        if recipy['id'] == res_id:
+#            x = res_id - 1
+#            return jsonify({'recipy': recipies[x]})
+#        else:
+#            abort(404)
 
 @app.route('/pers/api/v1/list/<int:res_id>', methods=['GET'])
 def list_recipy_by_id(res_id):
-    for recipy in recipies:
-        if recipy['id'] == res_id:
-            x = res_id - 1
-            return jsonify({'recipy': recipies[x]})
-        else:
-            abort(404)
-
-def list_recipy_by_id(res_id):
-    for recipy in recipies:
-        if recipy['id'] == res_id:
-            return recipy
-        else:
-            abort(404)
+    recipy = [recipy for recipy in recipies
+        if recipy['id'] == res_id]
+    if len(recipy) == 0:
+        abort(404)
+    elif recipy != None:
+        return jsonify(recipy)
+    else:
+        abort(404)
 
 
 
